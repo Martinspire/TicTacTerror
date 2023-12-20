9635ea42a85eaf8465ca7cc7677af7a7eac61dd7
Manon Schreuder <m.schreuder@jex.nl>
2023-12-20 16:08:15 +0100

    frontend setup

---
+++ frontend/TicTacTreats/src/components/PixiComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Application } from "vue3-pixi";
+import textureUrl from "@/assets/logo.svg";
+import { Rectangle } from "pixi.js";
+
+const hitArea = new Rectangle(0, 0, 64, 64);
+
+function onClick() {
+  console.log('sprite clicked!');
+}
+</script>
+
+<template>
+  <Application :width="1040" :height="800">
+    <text :anchor="0.5" :x="80" :y="20" :style="{ fill: 'white' }">
+      Tic Tac Treats
+    </text>
+    <sprite :x="120" :y="120" :texture="textureUrl" :hit-area="hitArea" @click="onClick" />
+  </Application>
+</template>
---
+++ frontend/TicTacTreats/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Pixi from '../components/PixiComponent.vue'
+</script>
+
+<template>
+  <main>
+    <Pixi />
+  </main>
+</template>
